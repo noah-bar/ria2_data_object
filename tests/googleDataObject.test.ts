3851116636c560bda97d4af9f512648097838732
noah-bar <noah.barberini@eduvaud.ch>
2024-01-09 12:04:49 +0100

    test: Add publish method tests

--- tests/googleDataObject.test.ts
+++ tests/googleDataObject.test.ts
@@ -2,6 +2,7 @@ import GoogleDataObject from "../src/GoogleDataObject";
 import dotenv from "dotenv"
 import {resolve} from 'path'
 import * as fs from "fs";
+import {ObjectNotFoundException} from "../src/exceptions/dataObjectExceptions";
 dotenv.config()
 
 const bucketName = process.env.BUCKET_NAME as string
@@ -90,5 +91,74 @@ describe("Test remove method", () => {
         //then
         expect(await dataObject.doesExist("testFolder")).toBeFalsy()
         expect(await dataObject.doesExist("testFolder/testFileInSubfolder")).toBeFalsy()
+    })
+})
+
+describe("Test download method", () => {
+    const localPath = resolve("./data/testDownloadedFile")
+
+    //Remove downloadedFile
+    afterEach(() => {
+        if(fs.existsSync(localPath)) {
+            fs.rmSync(localPath)
+        }
+    })
+
+    it("Should download remote file", async () => {
+        //given
+        await uploadTestFile()
+        expect(await dataObject.doesExist("testFile")).toBeTruthy()
+        expect(fs.existsSync(localPath)).toBeFalsy()
+
+        //when
+        await dataObject.download("testFile", localPath)
+
+        //then
+        expect(fs.existsSync(localPath)).toBeTruthy()
+    })
+
+    it("Should throw error if object is missing", async() => {
+        //given
+        expect(await dataObject.doesExist(localPath)).toBeFalsy()
+        expect(fs.existsSync(localPath)).toBeFalsy()
+
+        //then
+        await expect(dataObject.download("testFile", localPath)).rejects.toThrow(ObjectNotFoundException)
+    })
+})
+
+describe("Test publish method", () => {
+    const localPath = resolve("./data/testPublishedFile")
+
+    //Remove downloadedFile
+    afterEach(() => {
+        if(fs.existsSync(localPath)) {
+            fs.rmSync(localPath)
+        }
+    })
+
+    it("Should download remote file with url", async () => {
+        //given
+        await uploadTestFile()
+        expect(await dataObject.doesExist("testFile")).toBeTruthy()
+        expect(fs.existsSync(localPath)).toBeFalsy()
+
+        //when
+        const url = await dataObject.publish("testFile")
+        const res = await fetch(url, {})
+        const blob = await res.blob()
+        fs.writeFileSync(localPath, Buffer.from(await blob.arrayBuffer()))
+
+        //then
+        expect(fs.existsSync(localPath)).toBeTruthy()
+    })
+
+    it("Should throw error if object is missing", async() => {
+        //given
+        expect(await dataObject.doesExist(localPath)).toBeFalsy()
+        expect(fs.existsSync(localPath)).toBeFalsy()
+
+        //then
+        await expect(dataObject.publish("testFile")).rejects.toThrow(ObjectNotFoundException)
     })
 })
