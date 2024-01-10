db248de310db2563ebc1f51116535dca12b849f3
noah-bar <noah.barberini@eduvaud.ch>
2024-01-10 15:37:01 +0100

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import { resolve } from "path";
 import GoogleDataObject from "./GoogleDataObject";
-import IDataObject, { Url } from "./IDataObject";
-export {
-  GoogleDataObject,
-  IDataObject,
-  Url
-}
+import express, { Express, Router, Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = 3000
+const app = express()
+const router = Router()
+
+const keyFilename = resolve("./config/key.json")
+const bucketName = process.env.BUCKET_NAME as string
+const googleLabelDetector = new GoogleDataObject(keyFilename, bucketName)
+
+app.use(express.json())
+
+app.use("/api/v1", router)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
